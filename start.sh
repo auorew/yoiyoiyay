b01eb71e9028dea71a11085373caab9a2a26e944
alyssurite <149931727+alyssurite@users.noreply.github.com>
2026-01-19 04:49:14 +0300

    Fix using textual+memray

--- start.sh
+++ start.sh
@@ -20,39 +20,32 @@ monitor_memory() {
 
 monitor_memory &
 
-# starting bot
-echo "[$(date +'%Y-%m-%d %H:%M:%S')] INFO: Starting bot with memray..."
-poetry run memray run --live-remote --native main.py &
-PYTHON_PID=$!
-sleep 5
-
-echo "[$(date +'%Y-%m-%d %H:%M:%S')] INFO: Bot initializing... waiting 15s."
-sleep 15
-
 # starting textual
-echo "[$(date +'%Y-%m-%d %H:%M:%S')] INFO: Starting textual..."
-textual serve --port 5000 --host 0.0.0.0 "memray live 5000" &
+echo "[$(date +'%Y-%m-%d %H:%M:%S')] INFO: Starting server with textual..."
+poetry run textual serve --port 5000 --host 0.0.0.0 "memray live 1337" &
 TEXTUAL_PID=$!
 sleep 5
 
+# starting bot
+echo "[$(date +'%Y-%m-%d %H:%M:%S')] INFO: Starting bot with memray..."
+poetry run memray run --live-remote --live-port 1337 --native main.py &
+MEMRAY_PID=$!
+sleep 5
+
+# wait...
+echo "[$(date +'%Y-%m-%d %H:%M:%S')] INFO: Bot initializing... waiting 15s."
+sleep 15
+
 # starting pot provider
-echo "[$(date +'%Y-%m-%d %H:%M:%S')] INFO: Starting POT provider..."
+echo "[$(date +'%Y-%m-%d %H:%M:%S')] INFO: Starting POT provider with node..."
 node /app/bgutil/build/main.js --host 0.0.0.0 --port 4416 >/app/node_provider.log 2>&1 &
 NODE_PID=$!
 
-# wait for pot init
-sleep 5
+echo "[$(date +'%Y-%m-%d %H:%M:%S')] INFO: All services launched."
 
-# check for crashes
-if ! kill -0 $NODE_PID >/dev/null 2>&1; then
-    echo "[$(date +'%Y-%m-%d %H:%M:%S')] FATAL: POT provider failed to start!"
-    echo "--- node.js error logs ---"
-    cat /app/node_provider.log
-    echo "--- file tree ---"
-    find /app -maxdepth 3 -not -path '*/.*'
-fi
+wait $MEMRAY_PID
 
 echo "[$(date +'%Y-%m-%d %H:%M:%S')] INFO: Services running"
-echo "[$(date +'%Y-%m-%d %H:%M:%S')] textual PID: $TEXTUAL_PID"
-echo "[$(date +'%Y-%m-%d %H:%M:%S')] bot PID:     $PYTHON_PID"
-echo "[$(date +'%Y-%m-%d %H:%M:%S')] POT PID:     $NODE_PID"
+echo "[$(date +'%Y-%m-%d %H:%M:%S')] INFO: textual PID: $TEXTUAL_PID"
+echo "[$(date +'%Y-%m-%d %H:%M:%S')] INFO: python PID:  $MEMRAY_PID"
+echo "[$(date +'%Y-%m-%d %H:%M:%S')] INFO: node PID:    $NODE_PID"
