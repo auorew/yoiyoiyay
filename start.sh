445ade93471505c1047bf264377f38b56292c1b8
alyssurite <149931727+alyssurite@users.noreply.github.com>
2026-01-20 03:02:02 +0300

    Start memray app as well

--- start.sh
+++ start.sh
@@ -3,20 +3,24 @@
 # starting bot
 echo "[$(date +'%Y-%m-%d %H:%M:%S')] INFO: Starting bot with memray..."
 poetry run memray3.14 run --live-remote --live-port 1337 --native main.py &
+POETRY_PID=$!
+sleep 2
+
+# starting memray
+echo "[$(date +'%Y-%m-%d %H:%M:%S')] INFO: Starting memray..."
+memray3.14 live 1337 &
 MEMRAY_PID=$!
 sleep 2
 
 # starting pot provider
 echo "[$(date +'%Y-%m-%d %H:%M:%S')] INFO: Starting POT provider with node..."
 node /app/bgutil/build/main.js --host 0.0.0.0 --port 4416 >/app/node_provider.log 2>&1 &
-NODE_PID=$!
+BGUTIL_PID=$!
 sleep 2
 
 echo "[$(date +'%Y-%m-%d %H:%M:%S')] INFO: All services launched."
 
-wait $MEMRAY_PID
-
 echo "[$(date +'%Y-%m-%d %H:%M:%S')] INFO: Services running"
-echo "[$(date +'%Y-%m-%d %H:%M:%S')] INFO: textual PID: $TEXTUAL_PID"
-echo "[$(date +'%Y-%m-%d %H:%M:%S')] INFO: python PID:  $MEMRAY_PID"
-echo "[$(date +'%Y-%m-%d %H:%M:%S')] INFO: node PID:    $NODE_PID"
+echo "[$(date +'%Y-%m-%d %H:%M:%S')] INFO: python PID:  $POETRY_PID"
+echo "[$(date +'%Y-%m-%d %H:%M:%S')] INFO: memray PID:  $MEMRAY_PID"
+echo "[$(date +'%Y-%m-%d %H:%M:%S')] INFO: bgutil PID:  $BGUTIL_PID"
