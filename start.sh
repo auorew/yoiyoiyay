6501c2757069608907775382ee320395faaf72ee
auorew <auuorew@gmail.com>
2026-02-01 04:58:58 +0300

    Update start script

--- start.sh
+++ start.sh
@@ -12,6 +12,20 @@ node /app/bgutil/build/main.js --host 0.0.0.0 --port 4416 >/app/node_provider.lo
 BGUTIL_PID=$!
 sleep 2
 
+echo "[$(date +'%Y-%m-%d %H:%M:%S')] INFO: Checking POT provider health on port 4416..."
+MAX_RETRIES=5
+COUNT=0
+
+while ! nc -z localhost 4416; do
+    sleep 1
+    COUNT=$((COUNT + 1))
+    if [ $COUNT -ge $MAX_RETRIES ]; then
+        echo "[$(date +'%Y-%m-%d %H:%M:%S')] ERROR: POT provider failed to start on port 4416"
+    fi
+done
+
+echo "[$(date +'%Y-%m-%d %H:%M:%S')] INFO: POT provider is UP and listening."
+
 echo "[$(date +'%Y-%m-%d %H:%M:%S')] INFO: All services launched."
 
 echo "[$(date +'%Y-%m-%d %H:%M:%S')] INFO: Services running"
