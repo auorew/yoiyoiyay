db82a08f5c4d625d79b6164664cc0113df706aef
alyssurite <149931727+alyssurite@users.noreply.github.com>
2026-03-04 06:32:40 +0300

    Log out from Telegram cloud server in local mode case

--- yoiyoi/app/main.py
+++ yoiyoi/app/main.py
@@ -46,6 +46,8 @@ async def start_app(mode: int = BotMode.WEBHOOK):
     (Path(os.getcwd()) / bot_settings.cache_dir).mkdir(parents=True, exist_ok=True)
     # run bot and web server together
     async with bot_application:
+        if bot_settings.local_server:
+            await bot_application.bot.log_out()
         await bot_application.initialize()
         await on_bot_init(bot_application)
         await bot_application.start()
